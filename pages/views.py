bb9f17cb3ff50bffe5033c6cade19f50f1fe8062
harshad bhatia <harshadbhatia@harshads-MacBook-Pro.local>
2014-11-18 10:44:34 +1100

    Cleaned and fixed tests

--- pages/views.py
+++ pages/views.py
@@ -1,34 +1,24 @@
 # -*- coding: utf-8 -*-
 from django.views.generic import DetailView, ListView
 
-try:
-    # Only import from platforms if it is a dependancy
-    from platforms import views as platforms_views
-
-    # Use platform mixin if platforms is found as a dependancy
-    PlatformListMixin = platforms_views.PlatformListMixin
-    PlatformDetailMixin = platforms_views.PlatformDetailMixin
-except ImportError:
-    PlatformDetailMixin = PlatformListMixin = object
-
 from .models import Page
 
 
-class AllPages(PlatformListMixin, ListView):
+class Pages(ListView):
     model = Page
     template_name = 'pages/page_list.html'
 
     def get_queryset(self):
-        return super(AllPages, self).get_queryset().filter(enabled=True)
+        return super(Pages, self).get_queryset().filter(enabled=True)
 
 
-class DetailPage(PlatformDetailMixin, DetailView):
+class Page(DetailView):
     model = Page
     template_name = 'pages/page_detail.html'
 
     def get_queryset(self):
-        return super(DetailPage, self).get_queryset().filter(enabled=True)
+        return super(Page, self).get_queryset().filter(enabled=True)
 
 
-all_pages = AllPages.as_view()
-detail = DetailPage.as_view()
+pages = Pages.as_view()
+page = Page.as_view()
--- tests/test_models.py
+++ tests/test_models.py
@@ -12,9 +12,12 @@ import os
 import shutil
 import unittest
 
+from django.contrib.contenttypes.models import ContentType
+
 from pages import models
 from templates.models import Template
-
+from menus.models import Menu, MenuItem, Link
+from menus.factories import MenuFactory, LinkFactory, MenuItemFactory
 
 
 class TestPublica_pages(unittest.TestCase):
@@ -37,7 +40,7 @@ class WidgetsTestCase(unittest.TestCase):
         self.t1, _ = Template.objects.get_or_create(
             name='pages/page_object.html', content='test')
 
-        models.Page.objects.create(
+        self.page, _ = models.Page.objects.get_or_create(
             title='This is my long page title',
             short_title='Page Short Title',
             slug='this-is-my-long-page-title',
@@ -49,14 +52,63 @@ class WidgetsTestCase(unittest.TestCase):
         )
 
     def test_page_creation(self):
-        page = models.Page.objects.get(slug='this-is-my-long-page-title')
 
-        self.assertEqual(page.title, 'This is my long page title')
-        self.assertEqual(page.short_title, 'Page Short Title')
-        self.assertEqual(page.slug, 'this-is-my-long-page-title')
-        self.assertEqual(page.enabled, True)
+        self.t1, _ = Template.objects.get_or_create(
+            name='pages/page_object.html', content='test')
+
+        self.page, _ = models.Page.objects.get_or_create(
+            title='This is my long page title',
+            short_title='Page Short Title',
+            slug='this-is-my-long-page-title',
+            enabled=True,
+            text='here goes the long text like a lorem ipsum or something',
+            featured=True,
+            template=self.t1,
+            preview_template=self.t1
+        )
+
+        self.assertEqual(self.page.title, 'This is my long page title')
+        self.assertEqual(self.page.short_title, 'Page Short Title')
+        self.assertEqual(self.page.slug, 'this-is-my-long-page-title')
+        self.assertEqual(self.page.enabled, True)
         self.assertEqual(
-            page.text,
+            self.page.text,
             'here goes the long text like a lorem ipsum or something'
         )
-        self.assertEqual(page.featured, True)
+        self.assertEqual(self.page.featured, True)
+
+
+class TestMenuTestCase(unittest.TestCase):
+    def setUp(self):
+        '''
+        Will crete a menu,
+        Will create a page and link it to content type
+        display menu using the template tag.
+        :return:
+        '''
+
+        self.t1, _ = Template.objects.get_or_create(
+            name='pages/page_test.html', content='test')
+
+        self.page, _ = models.Page.objects.get_or_create(
+            title='This is my long page title',
+            short_title='Page Short Title',
+            slug='this-is-my-long-page-title',
+            enabled=True,
+            text='here goes the long text like a lorem ipsum or something',
+            featured=True,
+            template=self.t1,
+            preview_template=self.t1
+        )
+
+        self.link = Link(title='Title1',
+                         content_type=ContentType.objects.get_for_model(self.page)
+        )
+
+        self.menu = MenuFactory(title='Title1', slug='Slug1', enabled=True)
+        self.menu_item = MenuItemFactory(menu=self.menu, link=self.link,
+                                                   order=0, is_featured=True,
+                                                   parent=None)
+
+        def test_nothing(self):
+            pass
