ea47337c8cbc755bfde42e2496169eca8098fe8f
Daryl Antony <daryl@commoncode.com.au>
2014-11-27 10:43:55 +1100

    verbose names

--- pages/models.py
+++ pages/models.py
@@ -3,19 +3,23 @@ from django.core.urlresolvers import reverse
 from django.db import models
 
 from entropy.mixins import (
-    SlugMixin, TitleMixin, TextMixin, ModifiedMixin, CreatedMixin, EnabledMixin
+    SlugUniqueMixin, TitleMixin, TextMixin, ModifiedMixin, CreatedMixin, EnabledMixin
 )
 from images.mixins import ImageMixin
 from templates.mixins import TemplateMixin
 
 
-class Page(TitleMixin, SlugMixin, ImageMixin, EnabledMixin, TextMixin, TemplateMixin):
+class Page(TitleMixin, SlugUniqueMixin, ImageMixin, EnabledMixin, TextMixin, TemplateMixin):
 
     # title
     # short_title
     # slug
     # enabled
     # text
+
+    class Meta:
+        verbose_name = 'Content Page'
+        verbose_name_plural = 'Content Pages with Page Content Views'
 
     def get_absolute_url(self):
         return reverse('pages_page', args=(self.slug, ))
